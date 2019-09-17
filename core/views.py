5bd71ab7b2893fdef45aff10b13315c0ab8fb818
Kim Wittenburg <codello@wittenburg.kim>
2019-09-17 18:34:23 +0200

    Add Filters

--- core/views.py
+++ core/views.py
@@ -46,16 +46,20 @@ class DayEvents:
 
 @xframe_options_exempt
 def probenplan(request):
+    all_events = request.GET.get('all', 'false').lower() in ['true', '1', 'yes', 'on']
+    black_and_white = request.GET.get('bw', 'false').lower() in ['true', '1', 'yes', 'on']
+    from_date = request.GET.get('from', None)
+    to_date = request.GET.get('to', None)
+
+    if from_date:
+        from_date = arrow.get(from_date)
+    if to_date:
+        to_date = arrow.get(to_date)
+
     url = os.getenv('PROBENPLAN_CALENDAR')
     calendar = Calendar(urlopen(url).read().decode())
     events = []
-    days = set()
-    timeline = calendar.timeline.start_after(arrow.now(tz=settings.TIME_ZONE))
-    for event in timeline:
-        for date in arrow.Arrow.range('day', event.begin, event.end):
-            days.add(date.floor('day'))
-    for day in sorted(days):
-
+    for day in get_relevant_days(request, calendar, from_date, to_date):
         my_day_events = []
         for event in calendar.timeline.on(day):
             if event.end == day:
@@ -73,9 +77,31 @@ def probenplan(request):
         day_events.events = sorted(my_day_events, key=lambda e: (not e.all_day, e.begin))
         events.append(day_events)
     return render(request, 'core/probenplan.html', {
+        'from_date': from_date,
+        'to_date': to_date,
+        'black_and_white': black_and_white,
+        'all_events': all_events,
         'today': arrow.now(tz=settings.TIME_ZONE),
         'events': events
     })
+
+
+def get_relevant_days(request, calendar, from_date, to_date):
+    all_events = request.GET.get('all', 'false').lower() in ['true', '1', 'yes', 'on']
+
+    days = set()
+    if all_events:
+        timeline = calendar.timeline
+    elif from_date:
+        timeline = calendar.timeline.start_after(from_date)
+    else:
+        timeline = calendar.timeline.start_after(arrow.now(tz=settings.TIME_ZONE))
+    for event in timeline:
+        if to_date and event.begin > to_date.ceil('day'):
+            break
+        for date in arrow.Arrow.range('day', event.begin, event.end):
+            days.add(date.floor('day'))
+    return sorted(days)
 
 
 def clean_location(location):
