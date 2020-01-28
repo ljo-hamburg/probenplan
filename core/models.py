fbd2c3d2aef7fd6e87978350e31251513c43a892
Kim Wittenburg <codello@wittenburg.kim>
2020-01-28 11:54:11 +0100

    Refactor Backend

--- core/models.py
+++ core/models.py
@@ -1,4 +1,34 @@
 from django.db import models
+import arrow
+
+
+class EventList:
+    """
+    An instance of this class represents a list of events that are associated with a specific date (most commonly a
+    single day).
+    """
+    day: arrow
+    events: list
+
+    def __init__(self, day=None, events=None):
+        """
+        Creates a new instance with the specified `day` and `events`.
+        :param day: An arrow instance identifying the day represented by the instance.
+        :param events: A collection of events. The event will be sorted automatically.
+        """
+        self.day = day
+        if events:
+            self.events = sorted(events, key=lambda e: (not e.all_day, e.begin))
+
+    @property
+    def has_timed_events(self):
+        """
+        Returns a boolean value indicating whether the `EventList` contains events that are not all-day events.
+        """
+        for event in self.events:
+            if not event.all_day:
+                return True
+        return False
 
 
 class Event(models.Model):
@@ -24,6 +54,36 @@ class Event(models.Model):
             return self.title + " (whole day)"
         else:
             return self.title + " ({:%d. %b %Y})".format(self.begin)
+
+    @staticmethod
+    def timeline(from_date=None, to_date=None, all_events=False):
+        """
+        Returns an array of `EventList` objects identifying all events in the specified time frame. If no time frame is
+        given, all future events are considered.
+        :param from_date: The earliest date for events to be included.
+        :param to_date: The latest date for events to be included.
+        :param all_events: If `True` `from_date` and `to_date` are ignored and all events (including past events) are
+         considered.
+        :return: An array of `EventList` objects identifying the relevant events.
+        """
+        events = Event.objects.all()
+        if not all_events and from_date:
+            events = events.filter(end__gte=arrow.get(from_date).datetime)
+        elif not all_events and not from_date:
+            events = events.filter(end__gte=arrow.now().datetime)
+        if not all_events and to_date:
+            events = events.filter(begin__lte=arrow.get(to_date).datetime)
+
+        day_events = {}
+        for event in events:
+            for date in arrow.Arrow.range('day', event.begin, event.end):
+                floored = date.floor('day')
+                if date == event.end:
+                    continue
+                if floored not in day_events:
+                    day_events[floored] = set()
+                day_events[floored].add(event)
+        return [EventList(day, events) for (day, events) in day_events.items()]
 
 
 class EventColorDescription(models.Model):
--- core/views.py
+++ core/views.py
@@ -4,26 +4,7 @@ from django.shortcuts import render, redirect
 from django.views.decorators.clickjacking import xframe_options_exempt
 
 from Probenplan import settings
-from core.models import Event, EventColorMapping, EventColorDescription
-
-
-class EventList:
-    """
-    An instance of this class represents a list of events that are associated with a specific date (most commonly a
-    single day).
-    """
-    day: arrow
-    events: list
-
-    @property
-    def has_timed_events(self):
-        """
-        Returns a boolean value indicating whether the `EventList` contains events that are not all-day events.
-        """
-        for event in self.events:
-            if not event.all_day:
-                return True
-        return False
+from core.models import Event, EventColorDescription
 
 
 @xframe_options_exempt
@@ -53,36 +34,12 @@ def index(request):
     from_date = request.GET.get('from', None)
     to_date = request.GET.get('to', None)
 
-    events = Event.objects.all()
-    if not all_events and from_date:
-        events = events.filter(begin__gte=arrow.get(from_date).datetime)
-    elif not all_events and not from_date:
-        events = events.filter(begin__gte=arrow.now().datetime)
-    if not all_events and to_date:
-        events = events.filter(end__lte=arrow.get(to_date).datetime)
-
-    day_events = {}
-    for event in events:
-        for date in arrow.Arrow.range('day', event.begin, event.end):
-            floored = date.floor('day')
-            if date == event.end:
-                continue
-            if floored not in day_events:
-                day_events[floored] = set()
-            day_events[floored].add(event)
-    event_lists = []
-    for (day, events) in day_events.items():
-        event_list = EventList()
-        event_list.day = day
-        event_list.events = sorted(events, key=lambda e: (not e.all_day, e.begin))
-        event_lists.append(event_list)
-    event_lists = sorted(event_lists, key=lambda l: l.day)
     return render(request, 'core/probenplan.html', {
         'from_date': arrow.get(from_date) if from_date else None,
         'to_date': arrow.get(to_date) if to_date else None,
         'black_and_white': black_and_white,
         'all_events': all_events,
         'today': arrow.now(tz=settings.TIME_ZONE),
-        'events': event_lists,
+        'events': Event.timeline(from_date=from_date, to_date=to_date, all_events=all_events),
         'color_descriptions': EventColorDescription.objects.order_by('index')
     })
